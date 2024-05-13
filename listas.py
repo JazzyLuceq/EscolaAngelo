2616d633d200ce59d8ea484788e9427fc448ffe7
Karl David Lins <162985408+JazzyLuceq@users.noreply.github.com>
2024-05-13 07:53:09 -0300

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,12 @@
+listaNomes = []
+
+listaNomes.append("Edson")
+listaNomes.append("Tamires")
+listaNomes.append("Sofia")
+
+print(listaNomes[1])
+
+for nome in listaNomes:
+    print(nome)
+    
+# teste
