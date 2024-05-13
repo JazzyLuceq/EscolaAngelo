4843084dfeba8107d71d251be3e28b1570dbcbf4
Karl David Lins <162985408+JazzyLuceq@users.noreply.github.com>
2024-05-13 07:51:19 -0300

    Create dicionario.py

---
+++ dicionario.py
@@ -0,0 +1,28 @@
+listaNomes = []
+
+listaNomes.append("Edson")
+listaNomes.append("Tamires")
+listaNomes.append("Sofia")
+
+print(listaNomes)
+print(listaNomes[1])
+
+for nome in listaNomes:
+    print(nome)
+
+# DICIONÁRIOS
+
+primeiroDicionario = {}
+
+primeiroDicionario['nome'] = "Edson"
+primeiroDicionario['tipo'] = 'professor'
+primeiroDicionario['numero'] = 9
+print(primeiroDicionario)
+print(primeiroDicionario["tipo"])
+
+# Anatomia
+# lista[indice] = Valor 
+# dicionario[chave] = Valor 
+
+# Revisado
+# 24/04/2024
