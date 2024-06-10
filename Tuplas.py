11375cafe24a2e4a545a896394f24b64c70d99f3
Karl David Lins <162985408+JazzyLuceq@users.noreply.github.com>
2024-06-10 09:08:51 -0300

    Add files via upload

---
+++ Tuplas.py
@@ -0,0 +1,13 @@
+lista = [] ["escola", 9, True, 0.5]
+dicionario = {"texto":"Angelo", "numero":7, "Situação":False, "décima": 1.4}
+
+tupla1 = ("Mendes", 1, True, 5.4)
+tupla2 = "rua", 4, False, 0.7
+
+print(tupla1[0])
+
+lista2 = list(tupla2)
+print(lista2)
+lista2[0]
+tupla3 = tuple(lista2)
+print(tupla3)
